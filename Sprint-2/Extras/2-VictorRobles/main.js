5d9130dcae25bc26c862ea65442dda193b3024f9
Daniela Bustamante <daniela.annabella05@gmail.com>
2022-05-23 18:43:47 -0300

    CambiosAntesDeLaClase

--- Sprint-2/Extras/2-VictorRobles/main.js
+++ Sprint-2/Extras/2-VictorRobles/main.js
@@ -5,4 +5,8 @@ var altura = 160;
 var concatenacion = nombre + " " + altura;
 
 var datos = document.getElementById("datos");
-datos.innerHTML = ;
+datos.innerHTML = `
+    <h1>Soy la caja de datos</h1>
+    <h2>Mi nombre es: ${nombre}</h2> 
+    <h3>Mido: ${altura} cm</h3>
+`;
