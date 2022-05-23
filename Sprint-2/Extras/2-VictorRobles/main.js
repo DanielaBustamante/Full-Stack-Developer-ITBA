3f585077acbde9e58c9c1df5c822733740a5172b
Daniela Bustamante <daniela.annabella05@gmail.com>
2022-05-23 16:33:25 -0300

    antesdelvideodevictor

---
+++ Sprint-2/Extras/2-VictorRobles/main.js
@@ -0,0 +1,8 @@
+// alert("Hola mundo");
+var nombre = "Daniela";
+var altura = 160;
+
+var concatenacion = nombre + " " + altura;
+
+var datos = document.getElementById("datos");
+datos.innerHTML = ;
